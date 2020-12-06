aa2b062788c979996c9805a50e4203b1b41b73bd
mahin651 <53258123+mahin651@users.noreply.github.com>
2020-12-06 18:22:30 +0530

    Create calculator.py

---
+++ calculator.py
@@ -0,0 +1,29 @@
+def add():
+    print("Enter first Number to be added: ")
+    a = int(input())
+    print("Enter Second Number to be added: ")
+    b = int(input())
+
+    print("Sum : ",a+b)
+
+def sub():
+    print("Enter first Number to be subtracted : ")
+    a = int(input())
+    print("Enter Second Number to be subtracted: ")
+    b = int(input())
+
+    print("Subtraction : ",a-b)
+
+def mul():
+    print("Enter first Number to be multiplied: ")
+    a = int(input())
+    print("Enter Second Number to be multiplied: ")
+    b = int(input())
+
+    print("Multiplication : ",a*b)
+
+def div():
+    print("Enter numerator to be divided: ")
+    a = int(input())
+    print("Enter denomirator except  zero: ")
+    b = int(input())
