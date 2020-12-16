cd0caa193e41e9d1d62ab7dda7307460e7088440
mahin651 <53258123+mahin651@users.noreply.github.com>
2020-12-16 17:26:57 +0530

    Create stack.py

---
+++ stack.py
@@ -0,0 +1,22 @@
+
+
+
+stack = []
+
+
+stack.append('mahin')
+stack.append('cse')
+stack.append('b2')
+
+print('Initial stack')
+print(stack)
+
+
+print('\nElements poped from stack:')
+print(stack.pop())
+print(stack.pop())
+print(stack.pop())
+
+print('\nStack after elements are poped:')
+print(stack)
+
