3daa9254c8b8922162a989d6548cc012c61e1226
mahin651 <53258123+mahin651@users.noreply.github.com>
2020-12-16 17:23:01 +0530

    Create queue.py

---
+++ queue.py
@@ -0,0 +1,21 @@
+
+queue = []
+
+
+queue.append('mahin')
+queue.append('cse')
+queue.append('b2')
+
+print("Initial queue")
+print(queue)
+
+
+print("\nElements removed from queue")
+print(queue.pop(0))
+print(queue.pop(0))
+print(queue.pop(0))
+
+print("\nQueue after removing elements")
+print(queue)
+
+
